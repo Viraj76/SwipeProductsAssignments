967be008ccb629d2a548c63b6bf2d87771d8cbeb
Viraj76 <virajg.7680@gmail.com>
2024-05-25 04:02:16 +0530

    get request to the end point for products

--- app/src/main/java/com/example/swipeproducts/data/remote/api/ProductsAPI.kt
+++ app/src/main/java/com/example/swipeproducts/data/remote/api/ProductsAPI.kt
@@ -1,9 +1,10 @@
 package com.example.swipeproducts.data.remote.api
 
+import com.example.swipeproducts.data.remote.dto.ProductsDTO
 import retrofit2.http.GET
 
 interface ProductsAPI {
 
     @GET("api/public/get")
-    suspend fun getProductsList()
+    suspend fun getProductsList() : ProductsDTO
 }

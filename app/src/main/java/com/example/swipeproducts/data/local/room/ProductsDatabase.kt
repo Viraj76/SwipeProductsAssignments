40384bf27a450bc8a2ef7b4124b2323b00963c4a
Viraj76 <virajg.7680@gmail.com>
2024-05-25 17:16:28 +0530

    forgot to extend with RoomDB

--- app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDatabase.kt
+++ app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.swipeproducts.data.local.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.swipeproducts.data.remote.dto.ProductsDTO
 import com.example.swipeproducts.domain.models.Product
 
 @Database(entities = [Product::class], version = 1)
-abstract class ProductsDatabase {
+abstract class ProductsDatabase  : RoomDatabase(){
     abstract val productsDao : ProductsDao
 }

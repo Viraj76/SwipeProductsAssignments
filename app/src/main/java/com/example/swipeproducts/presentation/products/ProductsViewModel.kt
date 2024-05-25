721caade8f02831671d9543177836e72c03bf74b
Viraj76 <virajg.7680@gmail.com>
2024-05-25 16:57:05 +0530

    created view model for ProductsFragment

--- app/src/main/java/com/example/swipeproducts/presentation/products/ProductsViewModel.kt
+++ app/src/main/java/com/example/swipeproducts/presentation/products/ProductsViewModel.kt
@@ -1,8 +1,41 @@
 package com.example.swipeproducts.presentation.products
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.swipeproducts.domain.models.Product
+import com.example.swipeproducts.domain.usecases.products.data_classes.ProductUseCases
+import com.example.swipeproducts.utils.Resource
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class ProductsViewModel : ViewModel() {
+class ProductsViewModel : ViewModel() , KoinComponent{
+
+    private val productUseCases : ProductUseCases by inject()
+
+    fun getProductList() {
+        viewModelScope.launch {
+            productUseCases.getProductList().collect{resource ->
 
 
+                when(resource){
+                    is Resource.Loading ->{
+                        Log.d("viewmodel", "loading")
+                    }
+                    is Resource.Success ->{
+                        val data  = resource.data
+                        for (i in data!!){
+                            Log.d("viewmodel",i.toString())
+                        }
+                    }
+                    is Resource.Error -> {
+                        Log.d("viewmodel",resource.message.toString())
+                    }
+
+                }
+
+            }
+        }
+    }
 }

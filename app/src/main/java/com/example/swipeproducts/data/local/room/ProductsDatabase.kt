69f9b10e0b7e0846821692c3134f4f594dec93a6
Viraj76 <virajg.7680@gmail.com>
2024-05-25 17:15:12 +0530

    Created Room Database

--- app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDatabase.kt
+++ app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDatabase.kt
@@ -1,4 +1,10 @@
 package com.example.swipeproducts.data.local.room
 
-class ProductsDatabase {
+import androidx.room.Database
+import com.example.swipeproducts.data.remote.dto.ProductsDTO
+import com.example.swipeproducts.domain.models.Product
+
+@Database(entities = [Product::class], version = 1)
+abstract class ProductsDatabase {
+    abstract val productsDao : ProductsDao
 }

dd695380f0ddd3bdc8e761fcee87319c7605e6c2
Viraj76 <virajg.7680@gmail.com>
2024-05-25 15:58:41 +0530

    created GetProductUsecases

--- app/src/main/java/com/example/swipeproducts/domain/usecases/GetProductList.kt
+++ app/src/main/java/com/example/swipeproducts/domain/usecases/GetProductList.kt
@@ -1,10 +1,28 @@
 package com.example.swipeproducts.domain.usecases
 
+import com.example.swipeproducts.domain.models.Product
 import com.example.swipeproducts.domain.repository.ProductsRepository
+import com.example.swipeproducts.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetProductList(
-    val productsRepository: ProductsRepository
+    private val productsRepository: ProductsRepository
 ) {
 
-    
+    operator fun invoke() : Flow<Resource<List<Product>>> = flow{
+
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(data = productsRepository.getProductList()))
+
+        }
+        catch (e : Exception){
+            emit(Resource.Error(message = e.message.toString()))
+        }
+
+    }.flowOn(Dispatchers.IO)
 }

5599d91fe1ccdc47733121b73024e2ef7c136f6d
Viraj76 <virajg.7680@gmail.com>
2024-05-25 17:17:04 +0530

    Data access object for Room data base

--- app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDao.kt
+++ app/src/main/java/com/example/swipeproducts/data/local/room/ProductsDao.kt
@@ -1,4 +1,19 @@
 package com.example.swipeproducts.data.local.room
 
-interface ProductsDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.swipeproducts.domain.models.Product
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface ProductsDao  {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(products : List<Product>)
+
+    @Query("SELECT * FROM Product")
+    fun getProducts() : Flow<List<Product>>
 }

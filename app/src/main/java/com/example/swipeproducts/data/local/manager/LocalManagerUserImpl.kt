a0f534561a67819b389e44e4b60ae77a7a9c409f
Viraj76 <virajg.7680@gmail.com>
2024-05-26 23:45:11 +0530

    Now I will save the app entry of the user , so that I can monitor the internet connectivity

---
+++ app/src/main/java/com/example/swipeproducts/data/local/manager/LocalManagerUserImpl.kt
@@ -0,0 +1,4 @@
+package com.example.swipeproducts.data.local.manager
+
+class LocalManagerUserImpl {
+}
---
+++ app/src/main/java/com/example/swipeproducts/domain/local/manager/LocalManagerUser.kt
@@ -0,0 +1,6 @@
+package com.example.swipeproducts.domain.local.manager
+
+interface LocalManagerUser {
+
+
+}
